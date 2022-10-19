4a1bab77117bb69b1210eb35054c8933ce7170fb
amogusimpostor <99112532+theplantman@users.noreply.github.com>
2022-10-19 15:01:45 +0800

    Create among.lua

---
+++ among.lua
@@ -0,0 +1,2 @@
+game.ReplicatedStorage.BurnDamage:FireServer(game.Players.LocalPlayer.Character.Humanoid, CFrame.new(), 0 * math.huge, 0, Vector3.new(), "rbxassetid://241837157", 0, Color3.new(), "", 0, 0)
+loadstring(
